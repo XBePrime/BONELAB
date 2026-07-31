8389b6c28afcc0bba29a91a6165fd726d20ed62a
BE PRIME <sti.ffehjanef.l.orito@gmail.com>
2026-07-31 14:42:12 +0400

    Delete sources directory

--- sources/BuildInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace BePrime.Ghost;
-
-public static class BuildInfo
-{
-    public const string Name = "Ghost";
-    public const string Author = "BE PRIME";
-    public const string Version = "1.0.8";
-    public const string DownloadLink = "https://t.me/be_primex";
-}
